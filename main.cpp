0429766bcff09ce18b35a1c5989c712a80ff808d
bh34e5 <b.hester0614@yahoo.com>
2025-05-13 21:45:13 -0400

    render colors with textures

--- main.cpp
+++ main.cpp
@@ -9,22 +9,43 @@
 #include <stddef.h>
 #include <stdio.h>
 
-typedef unsigned int uint;
-
 #define unreachable _unreachable(__FILE__, __LINE__)
 #define _unreachable(f, l) __unreachable(f, l)
 #define __unreachable(f, l) assert(0 && ("Unreachable at " #f #l))
+
+struct Context {
+    double xpos, ypos;
+};
+
+Context *get_context(GLFWwindow *window) {
+    assert(window != nullptr);
+    Context *ctx = static_cast<Context *>(glfwGetWindowUserPointer(window));
+
+    assert(ctx != nullptr);
+    return ctx;
+}
 
 void handle_error(int error, char const *description) {
     fprintf(stderr, "GLFW Error (%d)): %s\n", error, description);
 }
 
-void handle_framebuffer_size(GLFWwindow *, int width, int height) {
+void _handle_framebuffer_size(GLFWwindow *, int width, int height) {
     glViewport(0, 0, width, height);
 }
 
+void _handle_cursor_pos(GLFWwindow* window, double xpos, double ypos) {
+    Context *ctx = get_context(window);
+
+    ctx->xpos = xpos;
+    ctx->ypos = ypos;
+}
+
+GLFWframebuffersizefun handle_framebuffer_size = &_handle_framebuffer_size;
+GLFWcursorposfun handle_cursor_pos = &_handle_cursor_pos;
+
 #define FACE_COUNT (6)
-#define CUBE_INDEX_COUNT (FACE_COUNT * 2 * 3)
+#define TRIANGLE_VERT_COUNT (FACE_COUNT * (2 * 3))
+
 enum Face {
     WHITE,
     RED,
@@ -63,14 +84,14 @@ struct CubeModel {
 
     // will have to do some management of these to make sure we don't have
     // physically incapable motions
-    float xrot[3];
-    float yrot[3];
-    float zrot[3];
+    GLfloat xrot[3];
+    GLfloat yrot[3];
+    GLfloat zrot[3];
 
-    float radius;
+    GLfloat radius;
 };
 
-inline uint face_idx(Face face) {
+inline GLuint face_idx(Face face) {
     assert(face >= WHITE && face <= YELLOW);
     switch (face) {
     case WHITE:  return 0;
@@ -87,9 +108,9 @@ inline uint face_idx(Face face) {
 void init_cube(CubeModel *cube) {
     FaceIter iter = begin_iter_faces();
     while (!iter.done) {
-        uint idx = face_idx(iter.face);
-        for (uint r = 0; r < 3; ++r) {
-            for (uint c = 0; c < 3; ++c) {
+        GLuint idx = face_idx(iter.face);
+        for (GLuint r = 0; r < 3; ++r) {
+            for (GLuint c = 0; c < 3; ++c) {
                 cube->faces[idx][r][c] = iter.face;
             }
         }
@@ -98,21 +119,13 @@ void init_cube(CubeModel *cube) {
     }
 }
 
-struct Context {};
-
-Context *get_context(GLFWwindow *window) {
-    assert(window != nullptr);
-    Context *ctx = static_cast<Context *>(glfwGetWindowUserPointer(window));
-
-    assert(ctx != nullptr);
-    return ctx;
-}
-
 char const *vert_shader_text = R"""(
 #version 330 core
 
 in vec3 pos;
+in float tex_ind;
 out vec3 pos_v;
+out float tex_ind_v;
 
 uniform mat4 object;
 uniform mat4 camera;
@@ -122,6 +135,7 @@ void main() {
     vec4 pos_hom = vec4(pos, 1.0);
 
     pos_v = pos;
+    tex_ind_v = tex_ind;
     gl_Position = perspective * camera * object * pos_hom;
 }
 )""";
@@ -130,7 +144,10 @@ char const *frag_shader_text = R"""(
 #version 330 core
 
 in vec3 pos_v;
+in float tex_ind_v;
 out vec4 color_out;
+
+uniform sampler2D face_colors;
 
 void main() {
     vec3 mapped = (pos_v + 1.0) * 0.5;
@@ -140,43 +157,44 @@ void main() {
     if (abs(pos_v.y) > 0.4) ++c;
     if (abs(pos_v.z) > 0.4) ++c;
 
-    if (c > 1) color_out = vec4(0.2, 0.2, 0.2, 1.0);
-    else color_out = vec4(mapped, 1.0);
+    color_out = (c > 1)
+        ? vec4(0.2, 0.2, 0.2, 1.0)
+        : texture(face_colors, vec2(tex_ind_v, 0.0));
 }
 )""";
 
 struct Camera {
-    float tx, ty, tz;
-    float rotx, roty, rotz;
+    GLfloat tx, ty, tz;
+    GLfloat rotx, roty, rotz;
 };
 
-inline float row_mult(float lhs[16], float rhs[16], uint r, uint c)
+inline GLfloat row_mult(GLfloat lhs[16], GLfloat rhs[16], GLuint r, GLuint c)
 {
-    float val = 0.0f;
-    for (uint i = 0; i < 4; ++i) {
+    GLfloat val = 0.0f;
+    for (GLuint i = 0; i < 4; ++i) {
         val += lhs[r * 4 + i] * rhs[i * 4 + c];
     }
     return val;
 }
 
-void mat_mult(float lhs[16], float rhs[16], float res[16]) {
-    for (uint r = 0; r < 4; ++r) {
-        for (uint c = 0 ; c < 4; ++c) {
+void mat_mult(GLfloat lhs[16], GLfloat rhs[16], GLfloat res[16]) {
+    for (GLuint r = 0; r < 4; ++r) {
+        for (GLuint c = 0 ; c < 4; ++c) {
             res[r * 4 + c] = row_mult(lhs, rhs, r, c);
         }
     }
 }
 
-void mat_mult_left(float lhs[16], float rhs[16]) {
-    float res[16];
+void mat_mult_left(GLfloat lhs[16], GLfloat rhs[16]) {
+    GLfloat res[16];
     mat_mult(lhs, rhs, res);
 
     memmove(rhs, res, sizeof(res));
 }
 
-void get_camera(float camera[16], Camera const *cam) {
+void get_camera(GLfloat camera[16], Camera const *cam) {
     // init to identity
-    for (uint i = 0; i < 16; ++i) {
+    for (GLuint i = 0; i < 16; ++i) {
         camera[i] = 0.0f;
     }
 
@@ -187,28 +205,28 @@ void get_camera(float camera[16], Camera const *cam) {
 
     // define matrices
 
-    float rotx_mat[16] = {
+    GLfloat rotx_mat[16] = {
         1.0f, 0.0f,             0.0f,              0.0f,
         0.0f, cosf(-cam->rotx), -sinf(-cam->rotx), 0.0f,
         0.0f, sinf(-cam->rotx), cosf(-cam->rotx),  0.0f,
         0.0f, 0.0f,             0.0f,              1.0f,
     };
 
-    float roty_mat[16] = {
+    GLfloat roty_mat[16] = {
         cosf(-cam->roty),  0.0f, sinf(-cam->roty), 0.0f,
         0.0f,              1.0f, 0.0f,             0.0f,
         -sinf(-cam->roty), 0.0f, cosf(-cam->roty), 0.0f,
         0.0f,              0.0f, 0.0f,             1.0f,
     };
 
-    float rotz_mat[16] = {
+    GLfloat rotz_mat[16] = {
         cosf(-cam->rotz), -sinf(-cam->rotz), 0.0f, 0.0f,
         sinf(-cam->rotz), cosf(-cam->rotz),  0.0f, 0.0f,
         0.0f,             0.0f,              1.0f, 0.0f,
         0.0f,             0.0f,              0.0f, 1.0f,
     };
 
-    float translation_mat[16] = {
+    GLfloat translation_mat[16] = {
         1.0f, 0.0f, 0.0f, -cam->tx,
         0.0f, 1.0f, 0.0f, -cam->ty,
         0.0f, 0.0f, 1.0f, -cam->tz,
@@ -244,7 +262,7 @@ GLuint compile_shader(GLenum type, char const *text) {
     return shader;
 }
 
-GLuint compile_program(char const *vert, char const *frag) {
+GLuint compile_link_program(char const *vert, char const *frag) {
     GLuint program = glCreateProgram();
 
     GLuint vert_shader = compile_shader(GL_VERTEX_SHADER, vert);
@@ -272,54 +290,67 @@ GLuint compile_program(char const *vert, char const *frag) {
 }
 
 struct Vec3 {
-    float x, y, z;
+    GLfloat x, y, z;
 };
 
-void bind_cube_pos_data(float cube_radius, GLuint vbo, GLuint veo) {
-    GLfloat verts[8 * 3] = {
-        -1.0f, -1.0f, -1.0f,
-        -1.0f, -1.0f, +1.0f,
-        -1.0f, +1.0f, -1.0f,
-        -1.0f, +1.0f, +1.0f,
-        +1.0f, -1.0f, -1.0f,
-        +1.0f, -1.0f, +1.0f,
-        +1.0f, +1.0f, -1.0f,
-        +1.0f, +1.0f, +1.0f,
-    };
-    GLuint indices[CUBE_INDEX_COUNT] = {
-        0, 2, 6,
-        0, 6, 4,
-        4, 6, 7,
-        4, 7, 5,
-        6, 2, 3,
-        6, 3, 7,
-        2, 0, 1,
-        2, 1, 3,
-        0, 4, 5,
-        0, 5, 1,
-        5, 7, 3,
-        5, 3, 1,
+void bind_cube_pos_data(GLfloat cube_radius, GLuint vbo, GLuint veo) {
+    GLfloat verts[] = {
+        -1.0f, -1.0f, -1.0f, 0.0f,
+        -1.0f, +1.0f, -1.0f, 0.0f,
+        +1.0f, +1.0f, -1.0f, 0.0f,
+        -1.0f, -1.0f, -1.0f, 0.0f,
+        +1.0f, +1.0f, -1.0f, 0.0f,
+        +1.0f, -1.0f, -1.0f, 0.0f,
+        +1.0f, -1.0f, -1.0f, 1.0f,
+        +1.0f, +1.0f, -1.0f, 1.0f,
+        +1.0f, +1.0f, +1.0f, 1.0f,
+        +1.0f, -1.0f, -1.0f, 1.0f,
+        +1.0f, +1.0f, +1.0f, 1.0f,
+        +1.0f, -1.0f, +1.0f, 1.0f,
+        +1.0f, +1.0f, -1.0f, 2.0f,
+        -1.0f, +1.0f, -1.0f, 2.0f,
+        -1.0f, +1.0f, +1.0f, 2.0f,
+        +1.0f, +1.0f, -1.0f, 2.0f,
+        -1.0f, +1.0f, +1.0f, 2.0f,
+        +1.0f, +1.0f, +1.0f, 2.0f,
+        -1.0f, +1.0f, -1.0f, 3.0f,
+        -1.0f, -1.0f, -1.0f, 3.0f,
+        -1.0f, -1.0f, +1.0f, 3.0f,
+        -1.0f, +1.0f, -1.0f, 3.0f,
+        -1.0f, -1.0f, +1.0f, 3.0f,
+        -1.0f, +1.0f, +1.0f, 3.0f,
+        -1.0f, -1.0f, -1.0f, 4.0f,
+        +1.0f, -1.0f, -1.0f, 4.0f,
+        +1.0f, -1.0f, +1.0f, 4.0f,
+        -1.0f, -1.0f, -1.0f, 4.0f,
+        +1.0f, -1.0f, +1.0f, 4.0f,
+        -1.0f, -1.0f, +1.0f, 4.0f,
+        +1.0f, -1.0f, +1.0f, 5.0f,
+        +1.0f, +1.0f, +1.0f, 5.0f,
+        -1.0f, +1.0f, +1.0f, 5.0f,
+        +1.0f, -1.0f, +1.0f, 5.0f,
+        -1.0f, +1.0f, +1.0f, 5.0f,
+        -1.0f, -1.0f, +1.0f, 5.0f,
     };
 
-    for (uint i = 0; i < 8; ++i) {
-        verts[i * 3 + 0] = cube_radius * verts[i * 3 + 0];
-        verts[i * 3 + 1] = cube_radius * verts[i * 3 + 1];
-        verts[i * 3 + 2] = cube_radius * verts[i * 3 + 2];
+    GLuint total_count = sizeof(verts) / sizeof(*verts);
+    GLuint row_count = total_count / 4;
+
+    for (GLuint i = 0; i < row_count; ++i) {
+        verts[i * 4 + 0] *= cube_radius;
+        verts[i * 4 + 1] *= cube_radius;
+        verts[i * 4 + 2] *= cube_radius;
     }
 
     glBindBuffer(GL_ARRAY_BUFFER, vbo);
     glBufferData(GL_ARRAY_BUFFER, sizeof(verts), verts, GL_STATIC_DRAW);
-
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, veo);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices,
-                 GL_STATIC_DRAW);
 }
 
 void bind_cube_object_matrix(CubeModel const* model, int x, int y, int z,
                              GLint loc) {
-    float xrot = model->xrot[x];
-    float yrot = model->yrot[y];
-    float zrot = model->zrot[z];
+    GLfloat xrot = model->xrot[x];
+    GLfloat yrot = model->yrot[y];
+    GLfloat zrot = model->zrot[z];
 
     GLfloat xrot_mat[16] = {
         1.0f, 0.0f,       0.0f,        0.0f,
@@ -342,10 +373,14 @@ void bind_cube_object_matrix(CubeModel const* model, int x, int y, int z,
         0.0f,       0.0f,        0.0f, 1.0f,
     };
 
+    GLfloat xf = GLfloat(x);
+    GLfloat yf = GLfloat(y);
+    GLfloat zf = GLfloat(z);
+
     GLfloat mat[16] = {
-        1.0f, 0.0f, 0.0f, (x - 1.0f) * 2.0f * model->radius,
-        0.0f, 1.0f, 0.0f, (y - 1.0f) * 2.0f * model->radius,
-        0.0f, 0.0f, 1.0f, (z - 1.0f) * 2.0f * model->radius,
+        1.0f, 0.0f, 0.0f, (xf - 1.0f) * 2.0f * model->radius,
+        0.0f, 1.0f, 0.0f, (yf - 1.0f) * 2.0f * model->radius,
+        0.0f, 0.0f, 1.0f, (zf - 1.0f) * 2.0f * model->radius,
         0.0f, 0.0f, 0.0f, 1.0f,
     };
 
@@ -354,6 +389,21 @@ void bind_cube_object_matrix(CubeModel const* model, int x, int y, int z,
     mat_mult_left(zrot_mat, mat);
 
     glUniformMatrix4fv(loc, 1, GL_TRUE, mat);
+}
+
+void bind_cube_texture(CubeModel const *model, GLuint x, GLuint y, GLuint z,
+                       GLuint texture) {
+    GLubyte colors[6 * 3] = {};
+
+    for (GLuint i = 0; i < 6; ++i) {
+        colors[3 * i + 0] = 64 * (x + 1) - 1;
+        colors[3 * i + 1] = 64 * (y + 1) - 1;
+        colors[3 * i + 2] = 64 * (z + 1) - 1;
+    }
+
+    glBindTexture(GL_TEXTURE_2D, texture);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, 6, 1, 0, GL_RGB, GL_UNSIGNED_BYTE,
+                 colors);
 }
 
 int main() {
@@ -398,23 +448,26 @@ int main() {
 
     // callbacks
     glfwSetFramebufferSizeCallback(window, handle_framebuffer_size);
+    glfwSetCursorPosCallback(window, _handle_cursor_pos);
 
-    GLuint prog = compile_program(vert_shader_text, frag_shader_text);
+    GLuint prog = compile_link_program(vert_shader_text, frag_shader_text);
 
     glUseProgram(prog);
+    GLint obj_loc = glGetUniformLocation(prog, "object");
+    GLint cam_loc = glGetUniformLocation(prog, "camera");
+    GLint per_loc = glGetUniformLocation(prog, "perspective");
+    GLint pos_loc = glGetAttribLocation(prog, "pos");
+    GLint tex_ind_loc = glGetAttribLocation(prog, "tex_ind");
 
-    GLuint vao = 0;
+    GLuint vaos[27] = {};
     GLuint vbo = 0;
     GLuint veo = 0;
+    GLuint textures[27] = {};
 
-    glGenVertexArrays(1, &vao);
+    glGenVertexArrays(27, vaos);
     glGenBuffers(1, &vbo);
     glGenBuffers(1, &veo);
-
-    glBindVertexArray(vao);
-    glBindBuffer(GL_ARRAY_BUFFER, vbo);
-
-    GLint loc = 0;
+    glGenTextures(27, textures);
 
     // TODO(bhester): tune these...
     GLfloat near = 1.0f;
@@ -428,18 +481,36 @@ int main() {
         0.0f,                0.0f,                 -1.0f,                        0.0f,
     };
 
-    loc = glGetUniformLocation(prog, "perspective");
-    glUniformMatrix4fv(loc, 1, GL_TRUE, perspective_mat);
+    for (GLuint i = 0; i < 27; ++i) {
+        GLuint vao = vaos[i];
+        GLuint texture = textures[i];
 
-    loc = glGetAttribLocation(prog, "pos");
-    glVertexAttribPointer(loc, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float),
-                          (void const *)0);
-    glEnableVertexAttribArray(loc);
+        glBindVertexArray(vao);
+        glBindBuffer(GL_ARRAY_BUFFER, vbo);
 
-    bind_cube_pos_data(model->radius, vbo, veo);
+        glUniformMatrix4fv(per_loc, 1, GL_TRUE, perspective_mat);
+
+        glVertexAttribPointer(pos_loc, 3, GL_FLOAT, GL_FALSE,
+                              4 * sizeof(GLfloat), (void const *)0);
+        glEnableVertexAttribArray(pos_loc);
+
+        glVertexAttribPointer(tex_ind_loc, 1, GL_FLOAT, GL_FALSE,
+                              4 * sizeof(GLfloat),
+                              (void const *)(3 * sizeof(GLfloat)));
+        glEnableVertexAttribArray(tex_ind_loc);
+
+        bind_cube_pos_data(model->radius, vbo, veo);
+
+        glBindTexture(GL_TEXTURE_2D, texture);
+
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    }
 
     Camera cam = {};
-    float cam_mat[16];
+    GLfloat cam_mat[16];
 
     cam.tz = 4.0f;
     get_camera(cam_mat, &cam);
@@ -451,16 +522,20 @@ int main() {
     while (!glfwWindowShouldClose(window)) {
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-        loc = glGetUniformLocation(prog, "camera");
-        glUniformMatrix4fv(loc, 1, GL_TRUE, cam_mat);
+        glUniformMatrix4fv(cam_loc, 1, GL_TRUE, cam_mat);
 
-        loc = glGetUniformLocation(prog, "object");
         for (int x = 0; x < 3; ++x) {
             for (int y = 0; y < 3; ++y) {
                 for (int z = 0; z < 3; ++z) {
-                    bind_cube_object_matrix(model, x, y, z, loc);
-                    glDrawElements(GL_TRIANGLES, CUBE_INDEX_COUNT, GL_UNSIGNED_INT,
-                                   (GLvoid const *)0);
+                    int index = 3 * (3 * (x) + y) + z;
+                    GLuint vao = vaos[index];
+                    GLuint texture = textures[index];
+
+                    glBindVertexArray(vao);
+
+                    bind_cube_object_matrix(model, x, y, z, obj_loc);
+                    bind_cube_texture(model, x, y, z, texture);
+                    glDrawArrays(GL_TRIANGLES, 0, TRIANGLE_VERT_COUNT);
                 }
             }
         }
@@ -532,9 +607,10 @@ int main() {
         get_camera(cam_mat, &cam);
     }
 
+    glDeleteTextures(27, textures);
     glDeleteBuffers(1, &veo);
     glDeleteBuffers(1, &vbo);
-    glDeleteVertexArrays(1, &vao);
+    glDeleteVertexArrays(27, vaos);
 
     glfwDestroyWindow(window);
     glfwTerminate();
